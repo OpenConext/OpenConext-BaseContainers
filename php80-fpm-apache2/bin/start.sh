e84309aaecd6aa7c0c985aa4808514ff0aaae712
Dan <dan@hostatic.ro>
2023-04-12 17:34:43 +0300

    Improving the logging system

--- php80-fpm-apache2/bin/start.sh
+++ php80-fpm-apache2/bin/start.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-# Start PHP-FPM in the background, but still it will output messages to STDOUT
+# Start PHP-FPM in the background
 php-fpm &
 
-# Start Apache2 in the background, tail its logs so they get printed to STDOUT
+# Start Apache2 in the background
 apache2ctl start
-tail -f /var/log/apache2/*.log
+
+# Tail all the necessary logs
+tail -f /var/log/apache2/*.log /usr/local/var/log/php-fpm-*.log
