7d8c5a0b53e377b6438256134e753cc24fed3e33
Dan <dan@hostatic.ro>
2023-12-07 15:53:47 +0200

    Cleaning up the entrypoint script so we can bring in the new one

--- php82-apache2/conf/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Copy and import the haproxy certificate
-
-if [ -e /config/haproxy/haproxy.crt ]; then
-	cp /config/haproxy/haproxy.crt /usr/local/share/ca-certificates/
-	update-ca-certificates
-fi
-# Hand off to the CMD
-exec "$@"
