3bb0adab37f5d2a33cf0a4858fffbafee595daa0
Bas Zoetekouw <bas.zoetekouw@surf.nl>
2026-03-25 11:22:01 +0100

    New php 8.5 container

---
+++ php85-apache2/bin/entrypoint.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+set +e
+
+# Read the envars for Apache2
+source /etc/apache2/envvars
+
+# Run as an arbitrary user / group if the user asked for one. It needs
+# to be created first and stripped of the leading #
+if [[ -v APACHE_UID ]]; then
+	export APACHE_RUN_USER=$APACHE_UID
+	APACHE_UID_TO_CREATE=$(echo $APACHE_UID | sed 's/#//')
+	if [[ -v APACHE_GUID ]]; then
+		export APACHE_RUN_GROUP=$APACHE_GUID
+		APACHE_GUID_TO_CREATE=$(echo $APACHE_GUID | sed 's/#//')
+		[ $(getent group openconext) ] || groupadd -g $APACHE_GUID_TO_CREATE openconext
+		[ $(getent passwd openconext) ] || useradd -M -u $APACHE_UID_TO_CREATE -g $APACHE_GUID_TO_CREATE openconext
+	else
+		[ $(getent passwd openconext) ] || useradd -M -u $APACHE_UID_TO_CREATE openconext
+	fi
+fi
+
+# Clear the PHP cache
+if [[ -f "/var/www/html/bin/console" ]]; then
+	php /var/www/html/bin/console cache:clear -e ${APP_ENV:-prod}
+fi
+
+# Create a list of directories to be chowned
+apache_dirs=("$APACHE_RUN_DIR" "$APACHE_LOG_DIR")
+if [[ -d "/var/www/html/var/cache" ]]; then
+	apache_dirs+=("/var/www/html/var/cache" "/var/www/html/var/log")
+	mkdir -p /var/www/html/var/log/
+fi
+if [[ -d "/var/www/html/app/cache" ]]; then
+	apache_dirs+=("/var/www/html/app/cache" "/var/www/html/app/logs")
+	mkdir -p /var/www/html/app/logs/
+fi
+
+for dir in "${apache_dirs[@]}"; do
+	if [[ -v APACHE_UID_TO_CREATE ]]; then
+		if [[ -v APACHE_GUID_TO_CREATE ]]; then
+			chown -R "$APACHE_UID_TO_CREATE:$APACHE_GUID_TO_CREATE" "$dir"
+		else
+			chown -R "$APACHE_UID_TO_CREATE" "$dir"
+		fi
+	else
+		chown -R "$APACHE_RUN_USER:$APACHE_RUN_GROUP" "$dir"
+	fi
+	chmod 1777 "$dir"
+done
+
+# If we do not define the HTTPD_CSP env var, let's set an empty one so
+# Apache stops complaining in the logs
+if [[ ! -v HTTPD_CSP ]]; then
+	export HTTPD_CSP=''
+fi
+
+# Copy and import the haproxy certificate if it exists
+if [ -e /config/haproxy/haproxy.crt ]; then
+	cp /config/haproxy/haproxy.crt /usr/local/share/ca-certificates/
+	update-ca-certificates
+fi
+
+# Hand off to CMD
+exec "$@"
